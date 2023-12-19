e300c5ff3148b2732a50394ef13efdae3a9b6757
panlm <stevenpan@gmail.com>
2023-12-19 15:11:45 +0800

    Update main.tf
    add metrics-server

--- multi-cluster-thanos/ekscluster2/main.tf
+++ multi-cluster-thanos/ekscluster2/main.tf
@@ -26,6 +26,15 @@ provider "helm" {
   }
 }
 
+resource "helm_release" "metrics-server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace   = "kube-system"
+
+  values = []
+}
+
 module "eks_cluster" {
   source = "../modules/eks_cluster"
 
