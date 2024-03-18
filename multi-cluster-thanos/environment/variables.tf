2e92e04bc7f85febb38f2228ebf14221ff8b584e
panlm <panlm@amazon.com>
2024-03-18 12:24:09 +0800

    Update variables.tf

--- multi-cluster-thanos/environment/variables.tf
+++ multi-cluster-thanos/environment/variables.tf
@@ -13,7 +13,7 @@ variable "aws_region" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.10.0.0/16"
 }
 
 variable "argocd_secret_manager_name_suffix" {
